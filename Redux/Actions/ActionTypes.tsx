61eec504324b70e3673d5e9cce4f9b1686228bc4
Godswill Edet <gimmex1@gmail.com>
2022-01-05 12:36:04 +0100

    Implemented sorting of new products

--- Redux/Actions/ActionTypes.tsx
+++ Redux/Actions/ActionTypes.tsx
@@ -1,19 +1,33 @@
 export enum ActionTypes {
-  GET_PRODUCTS = "GET_PRODUCTS",
+  // GET_PRODUCTS = "GET_PRODUCTS",
   GET_PRODUCTS_SUCCESS = "GET_PRODUCTS_SUCCESS",
-  GET_PRODUCTS_ERROR = "GET_PRODUCTS_ERROR",
+  // GET_PRODUCTS_ERROR = "GET_PRODUCTS_ERROR",
 }
 
-interface GetProducts {
-  type: ActionTypes.GET_PRODUCTS;
-}
+// interface GetProducts {
+//   type: ActionTypes.GET_PRODUCTS;
+// }
 interface GetProductsSuccess {
   type: ActionTypes.GET_PRODUCTS_SUCCESS;
-  payload: {}[];
+  payload: {
+    id: number;
+    slug: string;
+    name: string;
+    image: object;
+    category: string;
+    categoryImage: object;
+    new: true;
+    price: number;
+    description: string;
+    features: string;
+    includes: {}[];
+    gallery: object;
+    others: {}[];
+  }[];
 }
-interface GetProductsError {
-  type: ActionTypes.GET_PRODUCTS_ERROR;
-  payload: string;
-}
+// interface GetProductsError {
+//   type: ActionTypes.GET_PRODUCTS_ERROR;
+//   payload: string;
+// }
 
-export type Actions = GetProducts | GetProductsSuccess | GetProductsError;
+export type Actions = GetProductsSuccess;
--- Redux/Reducers/productsReducer.ts
+++ Redux/Reducers/productsReducer.ts
@@ -1,14 +1,28 @@
 import { Actions, ActionTypes } from "../Actions/ActionTypes";
 
 interface ProductsState {
-  loading: boolean;
-  error: string | null;
-  products: {}[];
+  // loading: boolean;
+  // error: string | null;
+  products: {
+    id: number;
+    slug: string;
+    name: string;
+    image: object;
+    category: string;
+    categoryImage: object;
+    new: true;
+    price: number;
+    description: string;
+    features: string;
+    includes: {}[];
+    gallery: object;
+    others: {}[];
+  }[];
 }
 
 const initialState = {
-  loading: true,
-  error: null,
+  // loading: true,
+  // error: null,
   products: [],
 };
 
@@ -17,14 +31,14 @@ const productsReducer = (
   action: Actions
 ): ProductsState => {
   switch (action.type) {
-    case ActionTypes.GET_PRODUCTS:
-      return { ...state, loading: true };
+    // case ActionTypes.GET_PRODUCTS:
+    //   return { ...state, loading: true };
 
     case ActionTypes.GET_PRODUCTS_SUCCESS:
-      return { ...state, loading: false, products: action.payload };
+      return { ...state, products: action.payload };
 
-    case ActionTypes.GET_PRODUCTS_ERROR:
-      return { ...state, loading: false, error: action.payload };
+    // case ActionTypes.GET_PRODUCTS_ERROR:
+    //   return { ...state, loading: false, error: action.payload };
 
     default:
       return state;
