c34c88e7838b6310daffb16adaf69ffff6c62a85
Godswill Edet <godswill@zimmerman.com>
2022-01-10 14:22:27 -0800

    Added carousel routing

--- components/Carousel/SwiperjsCarousel.tsx
+++ components/Carousel/SwiperjsCarousel.tsx
@@ -5,6 +5,11 @@ import "swiper/css/pagination";
 import "swiper/css/navigation";
 import SwiperCore, { Autoplay, Pagination, Navigation } from "swiper";
 import { ProductsProps } from "../../interfaces/interfaces";
+import styled from "styled-components";
+import { colors } from "../../Utils/Theme";
+import Button from "../Buttons/Button";
+import Link from "next/link";
+import Router from "next/router";
 SwiperCore.use([Autoplay, Pagination, Navigation]);
 
 export interface NewProductProps {
@@ -12,12 +17,12 @@ export interface NewProductProps {
 }
 const SwiperjsCarousel = ({ newProducts }: NewProductProps) => {
   return (
-    <div>
+    <SwiperjsCarouselStyles>
       <Swiper
         spaceBetween={30}
         centeredSlides={true}
         autoplay={{
-          delay: 2500,
+          delay: 4000,
           disableOnInteraction: false,
         }}
         pagination={{
@@ -29,18 +34,60 @@ const SwiperjsCarousel = ({ newProducts }: NewProductProps) => {
       >
         {newProducts.map((product) => (
           <SwiperSlide key={product.slug}>
-            <Image
-              width={300}
-              height={300}
-              src={product.image.desktop}
-              alt={`${product.name} image`}
-            />
-            <p>{product.name}</p>
+            <div>
+              <Image
+                width={300}
+                height={300}
+                src={product.image.desktop}
+                alt={`${product.name} image`}
+              />
+            </div>
+            <div>
+              <h1>{product.name}</h1>
+              <h1>{product.description}</h1>
+              <span onClick={() => Router.push(`/product/${product.slug}`)}>
+                <Button text="see product" variant="PINK_DARK" />
+              </span>
+            </div>
           </SwiperSlide>
         ))}
       </Swiper>
-    </div>
+    </SwiperjsCarouselStyles>
   );
 };
 
 export default SwiperjsCarousel;
+
+const SwiperjsCarouselStyles = styled.div`
+  color: #fff;
+  .swiper {
+    width: 100%;
+    height: 100%;
+  }
+
+  .swiper-slide {
+    text-align: center;
+    /* font-size: 18px; */
+    display: -webkit-box;
+    display: -ms-flexbox;
+    display: -webkit-flex;
+    display: flex;
+    -webkit-box-pack: center;
+    -ms-flex-pack: center;
+    -webkit-justify-content: center;
+    justify-content: center;
+    -webkit-box-align: center;
+    -ms-flex-align: center;
+    -webkit-align-items: center;
+    align-items: center;
+  }
+
+  .swiper-slide img {
+    display: block;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+  height: 63.2rem;
+  background-color: ${colors.colorBlack};
+`;
--- components/HomePage/Home.tsx
+++ components/HomePage/Home.tsx
@@ -11,62 +11,10 @@ const Home = ({ newProducts }: NewProductProps) => {
   return (
     <HomeStyles>
       <SwiperjsCarousel newProducts={newProducts} />
-      <div
-        style={{
-          margin: "3rem 1rem",
-        }}
-      >
-        <CategoryGroup />
-      </div>
-      <div
-        style={{
-          margin: "3rem 1rem",
-        }}
-      >
-        <ProductPreviewGroup />
-      </div>
-      <div
-        style={{
-          margin: "3rem 1rem",
-        }}
-      >
-        <BestAudio />
-      </div>
     </HomeStyles>
   );
 };
 
 export default Home;
 
-const HomeStyles = styled.div`
-  .swiper {
-    width: 100%;
-    height: 100%;
-  }
-
-  .swiper-slide {
-    text-align: center;
-    font-size: 18px;
-    background: #fff;
-
-    display: -webkit-box;
-    display: -ms-flexbox;
-    display: -webkit-flex;
-    display: flex;
-    -webkit-box-pack: center;
-    -ms-flex-pack: center;
-    -webkit-justify-content: center;
-    justify-content: center;
-    -webkit-box-align: center;
-    -ms-flex-align: center;
-    -webkit-align-items: center;
-    align-items: center;
-  }
-
-  .swiper-slide img {
-    display: block;
-    width: 100%;
-    height: 100%;
-    object-fit: cover;
-  }
-`;
+const HomeStyles = styled.div``;
